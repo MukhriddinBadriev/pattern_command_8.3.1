978633f02bf5a77199875935fbaf907e185b32a4
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2023-03-04 00:44:50 +0300

    Initial commit

---
+++ 8.3.1/8.3.1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+
+class PrintCommandConsole : public LogCommand {
+public:
+    void print(const std::string& message) override {
+        std::cout << message << std::endl;
+    }
+};
+
+class PrintCommandFile : public LogCommand {
+public:
+    void print(const std::string& message) override {
+        std::ofstream fout("Command.txt"); 
+        fout << message << std::endl;
+        fout.close();
+    }
+};
+
+void print(LogCommand& logcom) {
+    logcom.print("Hello world!");
+}
+
+int main()
+{
+    PrintCommandConsole cons;
+    PrintCommandFile file;
+
+    print(cons);
+    print(file);
+
+    return 0;
+}
